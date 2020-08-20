5ec347c27ffe09036af6a720cf34d66197f5ae79
Jorge Mario <jmferna91@gmail.com>
2020-08-19 20:34:38 -0400

    allow user to purchase any upgrade they wish if they have the money, cleaned up some more redundant code, etc...

--- src/components/Computer.js
+++ src/components/Computer.js
@@ -3,7 +3,6 @@ import CountTracker from "./CountTracker"
 import HireEmployees from "./HireEmployees"
 import EmployeePanel from "./EmployeePanel"
 import UpgradesPanel from "./UpgradesPanel"
-import ReactTooltip from 'react-tooltip'
 import "../App.css"
 
 const styles={
@@ -11,9 +10,10 @@ const styles={
 }
 
 function Computer(){
-
-    const [count, setCount] = useState(0)
-
+    // count keeps track of the total clicks that have been clicked
+    const [count, setCount] = useState(10000000)
+    
+    // sets and remembers the state of: cost, value, and nextCost of each employee
     const [interns, setInterns] = useState(0)
     const [internCost, setInternCost] = useState(15)
     const [internValue, setInternValue] = useState(0.1)
@@ -33,76 +33,32 @@ function Computer(){
     const [seniorCost, setSeniorCost] = useState(12000)  
     const [seniorValue, setSeniorValue] = useState(47) 
     const seniorNextCost = Math.ceil(12000 * (Math.pow(1.15, senior + 1)))
-
-    const [internUpgrades, setInternUpgrades] = useState(1)
-    const [internUpgradesCost, setInternUpgradeCost] = useState(100)
+    
+    // sets and remembers the state of: each upgrade
+    const [internUpgrade_1, setInternUpgrade_1] = useState(false)
+    const [internUpgrade_2, setInternUpgrade_2] = useState(false)
+    const [internUpgrade_3, setInternUpgrade_3] = useState(false)
  
-    const [juniorUpgrades, setJuniorUpgrades] = useState(1)
-    const [juniorUpgradesCost, setJuniorUpgradeCost] = useState(100000)
+    const [juniorUpgrade_1, setJuniorUpgrade_1] = useState(false)
+    const [juniorUpgrade_2, setJuniorUpgrade_2] = useState(false)
+    const [juniorUpgrade_3, setJuniorUpgrade_3] = useState(false)
 
-    const [midUpgrades, setMidUpgrades] = useState(1)
-    const [midUpgradesCost, setMidUpgradeCost] = useState(100000000)
+    const [midUpgrade_1, setMidUpgrade_1] = useState(false)
+    const [midUpgrade_2, setMidUpgrade_2] = useState(false)
+    const [midUpgrade_3, setMidUpgrade_3] = useState(false)
 
+    const [seniorUpgrade_1, setSeniorUpgrade_1] = useState(false)
+    const [seniorUpgrade_2, setSeniorUpgrade_2] = useState(false)
+    const [seniorUpgrade_3, setSeniorUpgrade_3] = useState(false)
 
-    const [seniorUpgrades, setSeniorUpgrades] = useState(1)
-    const [seniorUpgradesCost, setSeniorUpgradeCost] = useState(100000000000)
+    // functions to help make changes to states if necessary
+    let upgradeInterns = () => setInternValue(prevCount => prevCount * 2)
 
-    function upgradeInterns(){
-        setInternUpgrades(prevCount => prevCount + 1)
-        setCount(prevCount => prevCount - internUpgradesCost)
-        setInternUpgradeCost(prevCount => prevCount = internUpgradesCost*10)
-        setInternValue(prevCount => prevCount * 2)
-    }
+    let upgradeJuniors = () => setJuniorValue(prevCount => prevCount * 2)
 
-    function upgradeJuniors(){
-        setJuniorUpgrades(prevCount => prevCount + 1)
-        setCount(prevCount => prevCount - juniorUpgradesCost)
-        setJuniorUpgradeCost(prevCount => prevCount = juniorUpgradesCost*10)
-        setJuniorValue(prevCount => prevCount * 2)
-    }
+    let upgradeMids = () => setMidLevelValue(prevCount => prevCount * 2)
 
-    function upgradeMids(){
-        setMidUpgrades(prevCount => prevCount + 1)
-        setCount(prevCount => prevCount - midUpgradesCost)
-        setMidUpgradeCost(prevCount => prevCount = midUpgradesCost*10)
-        setMidLevelValue(prevCount => prevCount * 2)
-    }
-
-    function upgradeSeniors(){
-        setSeniorUpgrades(prevCount => prevCount + 1)
-        setCount(prevCount => prevCount - seniorUpgradesCost)
-        setSeniorUpgradeCost(prevCount => prevCount = seniorUpgradesCost*10)
-        setSeniorValue(prevCount => prevCount * 2)
-    }
-
-    function employeeToolTipMessage(employee){
-
-        var cpsTotal = 0
-        var cpsEach = 0
-
-        if (employee == "intern"){
-            cpsEach = internValue
-            cpsTotal = internCps().toFixed(1)
-        }
-        else if (employee == "junior"){
-            cpsEach = juniorValue
-            cpsTotal = juniorCps()
-        }
-        else if (employee == "mid"){
-            cpsEach = midLevelValue
-            cpsTotal = midCps()
-        }
-        else if (employee == "senior"){
-            cpsEach = seniorValue
-            cpsTotal = seniorCps()
-        }
-
-        // the array allows me to insert an page break, otherwise \n on it's own won't break up this line into two lines
-        const result = ["Each " + employee + " clicker produces " + cpsEach + " clicks per second", <br/> , "Total CPS: " + cpsTotal]
-
-        return result
-
-    }
+    let upgradeSeniors = () => setSeniorValue(prevCount => prevCount * 2)
 
     let internCps = () => interns*internValue
 
@@ -116,11 +72,12 @@ function Computer(){
 
     let incrementMouse = () => setCount(prevCount => prevCount + 1)
 
+    // helps to make the necessary state changes when an employee is bought
     function incrementInterns(){
         if (count >= internCost){
             setCount(prevCount => prevCount - internCost)
             setInterns(prevCount => prevCount + 1)
-            setInternCost(prevCount => prevCount = internNextCost)
+            setInternCost(internNextCost)
         }  
     }
 
@@ -128,7 +85,7 @@ function Computer(){
         if (count >= juniorCost){
             setCount(prevCount => prevCount - juniorCost)
             setJunior(prevCount => prevCount + 1)
-            setJuniorCost(prevCount => prevCount = juniorNextCost)
+            setJuniorCost(juniorNextCost)
         }
     }
 
@@ -136,7 +93,7 @@ function Computer(){
         if (count >= midLevelCost){
             setCount(prevCount => prevCount - midLevelCost)
             setMidLevel(prevCount => prevCount + 1)
-            setMidLevelCost(prevCount => prevCount = midLevelNextCost)
+            setMidLevelCost(midLevelNextCost)
         }
     }
 
@@ -144,10 +101,11 @@ function Computer(){
         if (count >= seniorCost){
             setCount(prevCount => prevCount - seniorCost)
             setSenior(prevCount => prevCount + 1)
-            setSeniorCost(prevCount => prevCount = seniorNextCost)
+            setSeniorCost(seniorNextCost)
         }
     }
 
+    // every second sets the count of the program to a new count depending on the cps of each employee bought
     useEffect(() => {
         setInterval(() => {
             setCount(prevCount => prevCount + internCps() + juniorCps() + midCps() + seniorCps())
@@ -156,8 +114,9 @@ function Computer(){
 
     const [shake, setShake] = useState(false)
 
+    // function which helps to shake the computer image for 1 second by setting the 
+    // state of shake to true then returning it to false
     function shakeImg(){
-        
         setInterval(() => {
             setShake(prevValue => true)
         }, [1000])
@@ -165,19 +124,18 @@ function Computer(){
     }
 
     return(
-        <div style={styles} className="row">
+        <div className="row">
             <div className="column">
                 {/* renders the clicks per second of all hired employees and displays it */}
                 <CountTracker count={count} cps={clicksPerSecond()}/>
                
-                <div style={styles} onClick={()=>shakeImg()}>
-                    {/* image of the computer */}
+                <div style={styles} onClick={()=>shakeImg()}> {/* image of the computer */}
                     <img className={shake ? "shake image-style" : "image-style"} src="http://pixelartmaker.com/art/96a034beedb086d.png" alt="Image of Computer" onClick={incrementMouse}></img>
                 </div>
             </div>
-            {/* middle panel that renders the individual employees hired */}
-            <div className="column" style={styles} style={{overflow: "auto", height:"auto", maxHeight:"80vh"}}>
 
+            {/* middle panel that renders the individual employees hired */}
+            <div className="column" style={{overflow: "auto", height:"auto", maxHeight:"80vh"}}>
                 <div style={styles} className="row" >
                     <EmployeePanel 
                         internCount={interns}
@@ -186,9 +144,9 @@ function Computer(){
                         seniorCount={senior}
                         />
                 </div>
-
             </div>
-            <div className="column" style={styles}>
+
+            <div className="column" >
                 <div style={styles} className="row" >
                     <p>Upgrades</p>
                     <UpgradesPanel 
@@ -196,29 +154,44 @@ function Computer(){
                         setCount={setCount}
                         
                         internCount={interns}
-                        internUpgrades={internUpgrades}
-                        upgradeInterns={()=>upgradeInterns}
+                        internUpgrade_1={internUpgrade_1}
+                        internUpgrade_2={internUpgrade_2}
+                        internUpgrade_3={internUpgrade_3}
+                        setInternUpgrade_1={()=>setInternUpgrade_1}
+                        setInternUpgrade_2={()=>setInternUpgrade_2}
+                        setInternUpgrade_3={()=>setInternUpgrade_3}
+                        upgradeInterns={()=>upgradeInterns()}
 
                         juniorCount={interns}
-                        juniorUpgrades={juniorUpgrades}
-                        upgradeJuniors={()=>upgradeJuniors}
+                        juniorUpgrade_1={juniorUpgrade_1}
+                        juniorUpgrade_2={juniorUpgrade_2}
+                        juniorUpgrade_3={juniorUpgrade_3}
+                        setJuniorUpgrade_1={()=>setJuniorUpgrade_1}
+                        setJuniorUpgrade_2={()=>setJuniorUpgrade_2}
+                        setJuniorUpgrade_3={()=>setJuniorUpgrade_3}
+                        upgradeJuniors={()=>upgradeJuniors()}
 
                         midCount={interns}
-                        midUpgrades={midUpgrades}
-                        upgradeMids={()=>upgradeMids}
+                        midUpgrade_1={midUpgrade_1}
+                        midUpgrade_2={midUpgrade_2}
+                        midUpgrade_3={midUpgrade_3}
+                        setMidUpgrade_1={()=>setMidUpgrade_1}
+                        setMidUpgrade_2={()=>setMidUpgrade_2}
+                        setMidUpgrade_3={()=>setMidUpgrade_3}
+                        upgradeMids={()=>upgradeMids()}
 
                         seniorCount={interns}
-                        seniorUpgrades={seniorUpgrades}
-                        upgradeSeniors={()=>upgradeSeniors}
+                        seniorUpgrade_1={seniorUpgrade_1}
+                        seniorUpgrade_2={seniorUpgrade_2}
+                        seniorUpgrade_3={seniorUpgrade_3}
+                        setSeniorUpgrade_1={()=>setSeniorUpgrade_1}
+                        setSeniorUpgrade_2={()=>setSeniorUpgrade_2}
+                        setSeniorUpgrade_3={()=>setSeniorUpgrade_3}
+                        upgradeSeniors={()=>upgradeSeniors()}
                         />
                 </div>
-                <div style={styles} >
+                <div >
                     <div className="HireEmployees">
-                        {/* data-tip will return tooltip string, which is managed by employeeToolTipMessage()
-                            data-for binds the contents of the element to a specific tooltip which is found below 
-                            className determines if element is greyed out or not depending if user can affor it*/}
-                            
-                            
                         {/* Render the Employees and their graphic, type, cost, and total */}
                         <HireEmployees 
                             count={count}
@@ -248,59 +221,6 @@ function Computer(){
                             incrementMidLevel={()=>incrementMidLevel}
                             incrementSenior={()=>incrementSenior}
                             />
-
-
-                        {/* <div data-tip="" data-for="internTip" className={count < internCost ?  "grey-out" : ""}>
-
-                            Render the Employees and their graphic, type, cost, and total
-                            <HireEmployees 
-                                incrementEmployee={()=>incrementInterns} 
-                                employeeType={"intern"}
-                                employeeCost={internCost}
-                                employeeTotal={interns}
-                                cps={internCps}
-                                />
-                        </div>
-                        <div data-tip="" data-for="juniorTip" className={count < juniorCost ?  "grey-out" : ""}>
-                            <HireEmployees 
-                                incrementEmployee={()=>incrementJunior} 
-                                employeeType={"junior"}
-                                employeeCost={juniorCost}
-                                employeeTotal={junior}
-                                cps={juniorCps}
-                                />
-                        </div>
-                        <div data-tip="" data-for="midTip" className={count < midLevelCost ?  "grey-out" : ""}>
-                            <HireEmployees 
-                                incrementEmployee={()=>incrementMidLevel} 
-                                employeeType={"mid"}
-                                employeeCost={midLevelCost}
-                                employeeTotal={midLevel}
-                                cps={midCps}
-                                />
-                        </div>
-                        <div data-tip="" data-for="seniorTip" className={count < seniorCost ?  "grey-out" : ""}>
-                            <HireEmployees 
-                                incrementEmployee={()=>incrementSenior} 
-                                employeeType={"senior"}
-                                employeeCost={seniorCost}
-                                employeeTotal={senior}
-                                cps={seniorCps}
-                                />
-                        </div> */}
-                        {/* binds a specific tooltip message to a specific element above */}
-                        {/* <ReactTooltip id="internTip" place="left" effect="solid">
-                            {employeeToolTipMessage("intern")}
-                        </ReactTooltip>
-                        <ReactTooltip id="juniorTip" place="left" effect="solid">
-                            {employeeToolTipMessage("junior")}
-                        </ReactTooltip>
-                        <ReactTooltip id="midTip" place="left" effect="solid">
-                            {employeeToolTipMessage("mid")}
-                        </ReactTooltip>
-                        <ReactTooltip id="seniorTip" place="left" effect="solid">
-                            {employeeToolTipMessage("senior")}
-                        </ReactTooltip> */}
                     </div>
                 </div>
             </div>
--- src/components/UpgradesPanel.js
+++ src/components/UpgradesPanel.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import ReactTooltip from 'react-tooltip'
 
 const styles = {
@@ -9,32 +9,14 @@ class UpgradesPanel extends React.Component{
 
     constructor(props){
         super()
+
         this.showUpgrades = this.showUpgrades.bind(this)
     }
 
-    showUpgrades(employee, upgradeLevel, cost) {
+    showUpgrades(employee, upgradeName, upgradeStatus, setUpgradeStatus, updateUpgrades, cost) {
 
-        let upgradeName = employee + "UpgradeTip-" + upgradeLevel
-        let updateUpgrades = () => null
-        let checkUpgrades = () => null
+        let upgradeTip = employee + "UpgradeTip-" + upgradeName
         let costMessage= cost
-
-        if (employee == "intern"){
-            updateUpgrades = () => this.props.upgradeInterns()
-            checkUpgrades = () => this.props.internUpgrades
-        }
-        else if (employee == "junior"){
-            updateUpgrades = () => this.props.upgradeJuniors()
-            checkUpgrades = () => this.props.juniorUpgrades
-        }
-        else if (employee == "mid"){
-            updateUpgrades = () => this.props.upgradeMids()
-            checkUpgrades = () => this.props.midUpgrades
-        }
-        else if (employee == "senior"){
-            updateUpgrades = () => this.props.upgradeSeniors()
-            checkUpgrades = () => this.props.seniorUpgrades
-        }
 
         if (cost/1000000000000 >= 1){
             costMessage = cost/1000000000000 + " Trillion"
@@ -48,30 +30,27 @@ class UpgradesPanel extends React.Component{
         else if (cost/1000 >= 1){
             costMessage = cost/1000 + " Thousand"
         }
-        
 
         return (    
             // check if upgrade has been bought before if yes then return null otherwise render the upgrade
-            checkUpgrades() <= upgradeLevel ?
+            upgradeStatus ? null :
 
             <span>
-                
-                <span   data-tip="" data-for={upgradeName}
-                        onClick={this.props.count >= cost ? updateUpgrades() : null} 
+                <span   data-tip="" data-for={upgradeTip}
+                        onClick={this.props.count >= cost ? () => (updateUpgrades(), setUpgradeStatus(true), this.props.setCount(prevCount => prevCount - cost)) : null} 
                         className={this.props.count < cost ?  "grey-out" : ""}>
                         
                         <img 
-                            src={require("./images/" + employee + "-upgrade-" + upgradeLevel + ".png")}  
+                            src={require("./images/" + employee + "-upgrade-" + upgradeName + ".png")}  
                             alt= "upgradeImg" 
                             style={{maxHeight: "35px"}}></img>
                 </span> 
                 
                 {/* shows the tool tip specific to the upgrade */}
-                <ReactTooltip id={employee + "UpgradeTip-" + upgradeLevel} place="bottom" effect="solid">
+                <ReactTooltip id={employee + "UpgradeTip-" + upgradeName} place="bottom" effect="solid">
                     Doubles the clicks per second of {employee} <br/> Cost: {costMessage} 
                 </ReactTooltip>
-                
-            </span> : null
+            </span> 
         )
     }
 
@@ -81,21 +60,23 @@ class UpgradesPanel extends React.Component{
                 <div className="row"> 
                     {/* calls the function which renders the upgrades specific for each employee type, 
                         upgrade level, and cost */}
-                    {this.showUpgrades("intern", 1, 100)}
-                    {this.showUpgrades("intern", 2, 1000)}
-                    {this.showUpgrades("intern", 3, 10000)}
 
-                    {this.showUpgrades("junior", 1, 100000)}
-                    {this.showUpgrades("junior", 2, 1000000)}
-                    {this.showUpgrades("junior", 3, 10000000)}
+                    {/* name, upgrade name, set upgrade status, update upgrades, cost */}
+                    {this.showUpgrades("intern", 1, this.props.internUpgrade_1, this.props.setInternUpgrade_1(), this.props.upgradeInterns, 100)}
+                    {this.showUpgrades("intern", 2, this.props.internUpgrade_2, this.props.setInternUpgrade_2(), this.props.upgradeInterns, 1000)}
+                    {this.showUpgrades("intern", 3, this.props.internUpgrade_3, this.props.setInternUpgrade_3(), this.props.upgradeInterns, 10000)}
 
-                    {this.showUpgrades("mid", 1, 100000000)}
-                    {this.showUpgrades("mid", 2, 1000000000)}
-                    {this.showUpgrades("mid", 3, 10000000000)}
-                    
-                    {this.showUpgrades("senior", 1, 100000000000)}
-                    {this.showUpgrades("senior", 2, 1000000000000)}
-                    {this.showUpgrades("senior", 3, 10000000000000)}
+                    {this.showUpgrades("junior", 1, this.props.juniorUpgrade_1, this.props.setJuniorUpgrade_1(), this.props.upgradeJuniors, 100000)}
+                    {this.showUpgrades("junior", 2, this.props.juniorUpgrade_2, this.props.setJuniorUpgrade_2(), this.props.upgradeJuniors, 1000000)}
+                    {this.showUpgrades("junior", 3, this.props.juniorUpgrade_3, this.props.setJuniorUpgrade_3(), this.props.upgradeJuniors, 10000000)}
+
+                    {this.showUpgrades("mid", 1, this.props.midUpgrade_1, this.props.setMidUpgrade_1(), this.props.upgradeMids, 100000000)}
+                    {this.showUpgrades("mid", 2, this.props.midUpgrade_2, this.props.setMidUpgrade_2(), this.props.upgradeMids, 1000000000)}
+                    {this.showUpgrades("mid", 3, this.props.midUpgrade_3, this.props.setMidUpgrade_3(), this.props.upgradeMids, 10000000000)}
+
+                    {this.showUpgrades("senior", 1, this.props.seniorUpgrade_1, this.props.setSeniorUpgrade_1(), this.props.upgradeSeniors, 100000000000)}
+                    {this.showUpgrades("senior", 2, this.props.seniorUpgrade_2, this.props.setSeniorUpgrade_2(), this.props.upgradeSeniors, 1000000000000)}
+                    {this.showUpgrades("senior", 3, this.props.seniorUpgrade_3, this.props.setSeniorUpgrade_3(), this.props.upgradeSeniors, 10000000000000)}
                 </div>
             </div>
         )
